c2af04617390172a1730ad44c2ae7e141da0435e
shixin.guo <shixin.guo@zoom.us>
2023-10-16 14:18:18 -0700

    remove web

--- packages/web/src/utils/date.ts
+++
@@ -1,7 +0,0 @@
-export const todayDate = (): string => {
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = String(today.getMonth() + 1).padStart(2, '0');
-  const day = String(today.getDate()).padStart(2, '0');
-  return `${year}-${month}-${day}`;
-};
